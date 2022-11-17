1aa3ed0d33c5ddbad639a35d1acdb2154b3ccd33
Eva Valls <evalls@webglstudio.gti.upf.edu>
2022-11-17 15:09:43 +0100

    shaderChunk included into IKHelper file and main.js renamed as app.js

--- js/IKHelper.js
+++ js/IKHelper.js
@@ -1,12 +1,11 @@
 import * as THREE from "three";
-import { ShaderChunk } from "./utils.js";
 
 class IKHelper {
 
     constructor(editor) {
 
         if(!editor)
-        throw("No editor to attach Gizmo!");
+        throw("No editor to attach Helper!");
 
         this.raycastEnabled = true;
 
@@ -326,5 +325,34 @@ class IKHelper {
     
 };
 
+const ShaderChunk = {
+
+	Point: {
+		vertexshader: `
+			attribute float size;
+			attribute vec3 color;
+			varying vec3 vColor;
+			void main() {
+				vColor = color;
+				vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+				gl_PointSize = size * ( 300.0 / -mvPosition.z );
+				gl_Position = projectionMatrix * mvPosition;
+			}
+		`,
+
+		fragmentshader: `
+			uniform vec3 color;
+			uniform sampler2D pointTexture;
+			uniform float alphaTest;
+			varying vec3 vColor;
+			void main() {
+				gl_FragColor = vec4(vColor, 1.0 );
+				gl_FragColor = gl_FragColor * texture2D( pointTexture, gl_PointCoord );
+				if ( gl_FragColor.a < alphaTest ) discard;
+			}
+		`
+	}
+
+};
 
 export { IKHelper };
